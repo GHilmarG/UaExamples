378a5963c43a1e32c6121af3674f027cb3aba40e
ghg <ghg@bas.ac.uk>
2017-04-10 13:20:57 +0100

    Tested with latest version

---
+++ RunAllExamples.m
@@ -0,0 +1,24 @@
+%%
+cd 1dIceShelf
+Ua
+cd ..
+close all
+
+cd 1dIceStream
+Ua
+cd ..
+close all
+
+cd GaussPeak
+Ua
+cd ..
+close all
+
+cd Inverse
+Ua
+cd ..
+close all
+
+cd MismipPlus
+Ua
+cd ..
