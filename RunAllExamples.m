b8ef5610f8ba85d7cc13ccf68f59d64d240c4a6d
ghg <ghg@bas.ac.uk>
2022-05-20 21:02:46 +0100

    turns out that the gradient-based matlab optimisation works fine with 2021b, so this is a 2022a issue

--- RunAllExamples.m
+++ RunAllExamples.m
@@ -34,15 +34,17 @@ Ua
 cd ..
 
 cd PIG-TWG\
-UserVar.RunType='Inverse-MatOpt'; Ua(UserVar) ;   % working 08/09/2021
-                                                  % Not working on20/05/2021 with alpha branch and 2022a. Need to look at
-                                                  % Error using message/getString
-                                                  % In 'optimlib:diagnose:HessFun', data type supplied is incorrect for parameter {0}.
-
 
 % UserVar.RunType='Inverse-UaOpt' ; Ua(UserVar) ;  % 08/09/2021 convergnece issues with the new Bedmachine based outline of Thwaites
 UserVar.RunType='Inverse-MatOpt' ;    CtrlVar.Inverse.MinimisationMethod="MatlabOptimization-HessianBased"; Ua(UserVar,CtrlVar) ;
-UserVar.RunType='Inverse-MatOpt' ;    CtrlVar.Inverse.MinimisationMethod="MatlabOptimization-GradientBased"; Ua(UserVar,CtrlVar) ;
+
+UserVar.RunType='Inverse-MatOpt' ;    CtrlVar.Inverse.MinimisationMethod="MatlabOptimization-GradientBased"; Ua(UserVar,CtrlVar) ;  
+                                     % Note on 2022-05-20. For some reason this no longer runs using the Matlab optimization toolbox with MATLAB2022a.
+                                     % This DOES work with MATLAB2021b
+                                     % The reasons are unclear, but for the time being gradient-based optimisation with the Matlab toolbox can not be done with MATLAB2022a.
+                                     % This is not too much of an issue as the default option is the HessianBased approach anyhonw, which is also the better option.
+
+
 UserVar.RunType='TestingMeshOptions' ; Ua(UserVar) ;
 cd ..
 close all
